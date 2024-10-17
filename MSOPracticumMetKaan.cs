d2568ce4c305949d2168fdf1c39e24d70fdd6efc
Moonblye <lars.van.dommelen@xs4all.nl>
2024-10-17 15:04:09 +0200

    Implemented almost every class/interface
    Implemented most of the classes and interfaces.

--- MSOPracticumMetKaan.cs
+++ MSOPracticumMetKaan.cs
@@ -1,12 +1,74 @@
 using System;
 
 namespace ScratchMini {
-    class program
+    class Main
     {
         static void Main()
         {
             
         }
     }
+    class Program
+    {
+        String name;
+        List <commands
+    }
+
+    interface ICommandLine
+    {
+        Program basicP;
+        Program advanced;
+        Program extreme;
+        Program loaded;
+        
+
+    }
+
+    class Player : GridObject
+    {
+        String image;
+        CardinalDirection cardinalDirection;
+    }
+
+    class Field{}
+
+    interface IGridObject{}
+    class EmptySpace : IGridObject{}
+
+    enum CardinalDirection
+    {
+        Nort,
+        South, 
+        East, 
+        West
+    }
+
+    interface ICommand
+    {
+        String name;
+        Field executeCommand(){}
+    }
+
+    class RepeatCommand : ICommand {
+    String name;
+    Int times;
+    List<ICommand> commands
+
+    public Field executeCommand(Field field){}
+
+    }
+
+    class TurnCommand : ICommand {
+        String name;
+        Char turn;
+        public Field executeCommand(Field field){}
+    }
+
+    class MoveCommand : ICommand {
+        String name;
+        Int steps;
+        public Field executeCommand(Field field)
+    }
+
 }
 
